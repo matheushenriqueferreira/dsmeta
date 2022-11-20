8fc8b0c1ecefafe7176377f0cf5abb694882d4c1
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-11-20 18:49:30 -0300

    Enviar notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios'
 import './styles.css'
 import btnIcon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
-const NotificationButton = () => {
+type Props = {
+  saleId: number;
+}
+
+const handleClick = (id : number) => {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then((response) => {
+    
+  })
+}
+
+const NotificationButton = ( {saleId} : Props ) => {
   return(
-    <div className='dsmeta-red-btn'>
+    <div className='dsmeta-red-btn' onClick={() => handleClick(saleId)}>
       <img src={btnIcon} alt="Notificação" />
     </div>
   )
